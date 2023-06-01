e7ef5e80762c54a1457c3da397a56092806665d5
unknown <osama98138139@gmail.com>
2023-06-01 11:18:00 +0400

    swap

---
+++ fith-project/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        for( int i = 0; i < 5; i++)
+        {
+            
+            
+
+            for( int j = 0; j < 8; j++) 
+            {
+                if(i == j)
+                {
+                    Console.Write("*");
+                }
+                
+            
+            }
+
+            Console.WriteLine("*");
+        
+        } 
+        
+    }
+}
---
+++ swap.program/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 6;
+        int b = 9;
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        
+        Console.WriteLine("a = "+a +" b = "+b);
+
+        String h = "Hello";
+        String j = "world1234";
+        h = h + j;
+        j = h.Substring(0, h.Length - j.Length);
+        h = h.Substring(j.Length);
+
+        
+        Console.WriteLine("h = " + h + " and j = " + j);
+
+    }
+}
