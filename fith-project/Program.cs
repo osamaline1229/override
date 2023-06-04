8890b1188a05beaf3bf73b58fc40072bcf4c4238
osamaline1229 <osama98138139@gmail.com>
2023-06-04 10:39:32 +0400

    print stars

--- fith-project/Program.cs
+++ fith-project/Program.cs
@@ -3,23 +3,23 @@
     private static void Main(string[] args)
     {
         
-        for( int i = 0; i < 5; i++)
+        for( int i = 0; i < 20; i++)
         {
             
             
 
-            for( int j = 0; j < 8; j++) 
+            for( int j = 0; j < 50; j++) 
             {
-                if(i == j)
-                {
-                    Console.Write("*");
-                }
+                
+                
+                 Console.Write("*");
+                
                 
             
             }
 
             Console.WriteLine("*");
-        
+
         } 
         
     }
