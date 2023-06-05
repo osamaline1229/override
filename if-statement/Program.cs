9f3b51ce4fc233e45a66370a7d48ecfff5d23936
osamaline1229 <osama98138139@gmail.com>
2023-06-05 10:13:47 +0400

    if advance

---
+++ if-statement/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 1, b = 1;
+        int c = 5;
+
+        if (a<b || c == 5)
+        {
+            Console.WriteLine("condition is met.");
+        }
+        else
+        {
+            Console.WriteLine("condition is not met.");
+        }
+         
+        //khjkhjk 
+    }
+}
