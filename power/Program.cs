a82a9420b02c35d543dea6e335d6f2786c8b626a
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-08 01:12:10 -0700

    error

--- power/Program.cs
+++ power/Program.cs
@@ -2,27 +2,28 @@
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main (string[] args)
     {
-        System.Console.WriteLine(power(3, 3));
+        System.Console.WriteLine( power(3, 3));
+
     }
 
-    //static int calculateFib(int n)
-    //{
-    //    int secondLastTerm = 0;
-    //    int lastTerm = 1;
-    //    int nextTerm = 0;
+    static int calculateFib(int n)
+    {
+        int secondLastTerm = 0;
+        int lastTerm = 1;
+        int nextTerm = 0;
 
-    //    for(int i = 2; i <= n; i++)
-    //    {
-    //        nextTerm = lastTerm + secondLastTerm;
-    //        secondLastTerm = lastTerm;
-    //        lastTerm = nextTerm;
-    //    }
-    //    return nextTerm;
-    //}
+        for (int i = 2; i <= n; i++)
+        {
+            nextTerm = lastTerm + secondLastTerm;
+            secondLastTerm = lastTerm;
+            lastTerm = nextTerm;
+        }
+        return nextTerm;
+    }
 
-    static int power (int num, int pow)
+    static int power(int num, int pow)
     {
         if(pow == 0)
         {
