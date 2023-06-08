256e231097076b68aef3aeb3e3fd0ac38e0478b6
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-08 01:00:28 -0700

    second recursion

--- power/Program.cs
+++ power/Program.cs
@@ -7,20 +7,20 @@ internal class Program
         System.Console.WriteLine(power(3, 3));
     }
 
-    static int calculateFib(int n)
-    {
-        int secondLastTerm = 0;
-        int lastTerm = 1;
-        int nextTerm = 0;
+    //static int calculateFib(int n)
+    //{
+    //    int secondLastTerm = 0;
+    //    int lastTerm = 1;
+    //    int nextTerm = 0;
 
-        for(int i = 2; i <= n; i++)
-        {
-            nextTerm = lastTerm + secondLastTerm;
-            secondLastTerm = lastTerm;
-            lastTerm = nextTerm;
-        }
-        return nextTerm;
-    }
+    //    for(int i = 2; i <= n; i++)
+    //    {
+    //        nextTerm = lastTerm + secondLastTerm;
+    //        secondLastTerm = lastTerm;
+    //        lastTerm = nextTerm;
+    //    }
+    //    return nextTerm;
+    //}
 
     static int power (int num, int pow)
     {
