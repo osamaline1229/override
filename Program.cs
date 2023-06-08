341b2bf08521c7915220c5723eeba8d8c87c309a
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-08 01:14:51 -0700

    try to fix

--- Program.cs
+++ Program.cs
@@ -1,38 +1,41 @@
-﻿using System.Globalization;
-
-internal class Program
+﻿namespace recursion
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        System.Console.WriteLine(power(3, 3));
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine(power(3, 3));
+        }
+
+        static int calculateFib(int n)
+        {
+            int secondLastTerm = 0;
+            int lastTerm = 1;
+            int nextTerm = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                nextTerm = lastTerm + secondLastTerm;
+                secondLastTerm = lastTerm;
+                lastTerm = nextTerm;
+            }
+            return nextTerm;
+        }
+
+        static int power(int num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            if (pow == 1)
+            {
+                return num;
+            }
+            return num * power(num, pow - 1);
+
+        }
     }
 
-    static int calculateFib(int n)
-    {
-        int secondLastTerm = 0;
-        int lastTerm = 1;
-        int nextTerm = 0;
-
-        for(int i = 2; i <= n; i++)
-        {
-            nextTerm = lastTerm + secondLastTerm;
-            secondLastTerm = lastTerm;
-            lastTerm = nextTerm;
-        }
-        return nextTerm;
-    }
-
-    static int power (int num, int pow)
-    {
-        if(pow == 0)
-        {
-            return 1;
-        }
-        if (pow == 1)
-        {
-            return num;
-        }
-        return num * power(num, pow - 1);
-
-    }
 }
+
