fec8c1133f5c2f261bd76f893c11a67c13054f35
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-07 22:42:35 -0700

    swap2

--- if-statement/Program.cs
+++ if-statement/Program.cs
@@ -2,18 +2,46 @@
 {
     private static void Main(string[] args)
     {
-        int a = 1, b = 1;
-        int c = 5;
+        //int a = 1, b = 1;
+        //int c = 5;
 
-        if (a<b || c == 5)
+        //if (a < b || c == 5)
+        //{
+        //    Console.WriteLine("condition is met.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("condition is not met.");
+        //}
+
+        //khjkhjk
+        //
+
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    for(int j = 0; j < i; j++)
+        //    {
+        //       Console.WriteLine(j);
+        //    }
+        //}
+
+        for(int i= 0; i <5; i++)
         {
-            Console.WriteLine("condition is met.");
+
+
+            Console.WriteLine("*");
+            
+            for (int j=0; j<=i; j++)
+            {
+                Console.Write("*");
+                
+            }
+            if ( == )
+            {
+                Console.WriteLine();
+            }
         }
-        else
-        {
-            Console.WriteLine("condition is not met.");
-        }
-         
-        //khjkhjk 
+
+
     }
 }
