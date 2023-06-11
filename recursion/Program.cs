1cbc2dce96cb46238c70befaea4fe19e454e4c0e
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-11 07:40:19 -0700

    power and factorial recursion

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -4,17 +4,17 @@
     {
         int answer = 0;
         float answer2 = 0;
-        //answer = fac(6);
+        answer = fac(6);
         answer2 = pow(3, 3);
-        Console.WriteLine(answer);
+        Console.WriteLine("the factorial "+ answer);
         Console.WriteLine("the answer is : "+answer2);
     }
 
-    //private static int fac(int n)
-    //{
-    //    if (n == 1) return 1;
-    //    return n * fac(n -1);
-    //}
+    private static int fac(int n)
+    {
+        if (n == 1) return 1;
+        return n * fac(n - 1);
+    }
 
     private static float pow(float num, int power)
     {
