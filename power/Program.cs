0f4e44a45744a85017de14275a4dfb88a94ff094
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-10 23:00:36 -0700

    patterns 1

--- power/Program.cs
+++ power/Program.cs
@@ -2,38 +2,41 @@
 
 internal class Program
 {
-    static void Main (string[] args)
-    {
-        System.Console.WriteLine( power(3, 3));
-
-    }
-
-    static int calculateFib(int n)
-    {
-        int secondLastTerm = 0;
-        int lastTerm = 1;
-        int nextTerm = 0;
-
-        for (int i = 2; i <= n; i++)
+        static void Main(string[] args)
         {
-            nextTerm = lastTerm + secondLastTerm;
-            secondLastTerm = lastTerm;
-            lastTerm = nextTerm;
-        }
-        return nextTerm;
-    }
+        System.Console.WriteLine(power(5, 5));
 
-    static int power(int num, int pow)
-    {
-        if(pow == 0)
-        {
-            return 1;
         }
-        if (pow == 1)
-        {
-            return num;
-        }
-        return num * power(num, pow - 1);
 
+    
+
+        static int calculateFib(int n)
+        {
+            int secondLastTerm = 0;
+            int lastTerm = 1;
+            int nextTerm = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                nextTerm = lastTerm + secondLastTerm;
+                secondLastTerm = lastTerm;
+                lastTerm = nextTerm;
+            }
+            return nextTerm;
+        }
+
+        static int power(int num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            if (pow == 1)
+            {
+                return num;
+            }
+            return num * power(num, pow - 1);
+
+        }
     }
 }
