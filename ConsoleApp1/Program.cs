09dd57f55e7e33f1030febca9d8bc0b8857a6e29
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-11 00:33:13 -0700

    array_days

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,5 +5,6 @@
         int[] a = new int[5];
         int[] a1 = { 1, 2, 3, 4, 5 };
         Console.WriteLine(a1[i]);
+        //Read and Write the Arrays
     }
 }
---
+++ arrays-main/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+        Console.WriteLine(weekDays2[0]);
+        Console.WriteLine(weekDays2[1]);
+        Console.WriteLine(weekDays2[2]);
+        Console.WriteLine(weekDays2[3]);
+        Console.WriteLine(weekDays2[4]);
+        Console.WriteLine(weekDays2[5]);
+        Console.WriteLine(weekDays2[6]);
+
+    }
+}
