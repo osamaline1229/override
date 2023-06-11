5981b33b270d0cea0bceed93374153182dd7bccd
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-10 23:25:09 -0700

    arrays1

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] a = new int[5] = { 1, 2, 3, 4} ;
+       
+        Console.WriteLine(a.Length);
+    }
+}
