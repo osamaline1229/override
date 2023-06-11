bd58998ac023eea4b5779247331d2cc7b56f8f83
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-11 01:22:08 -0700

    arrays

--- arrays-main/Program.cs
+++ arrays-main/Program.cs
@@ -2,15 +2,62 @@
 {
     private static void Main(string[] args)
     {
-        string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+        //int[] a = { 9, 2, 3, 8, 5, 6, 7 };
 
-        Console.WriteLine(weekDays2[0]);
-        Console.WriteLine(weekDays2[1]);
-        Console.WriteLine(weekDays2[2]);
-        Console.WriteLine(weekDays2[3]);
-        Console.WriteLine(weekDays2[4]);
-        Console.WriteLine(weekDays2[5]);
-        Console.WriteLine(weekDays2[6]);
+        //double sum = 0;
+        //double avg = 0;
+        //for(int i  = 0; i < a.Length; i++)
+        //{
+        //    sum = sum + a[i];
 
+        //}
+        //avg = sum / a.Length;
+        //Console.WriteLine("the average of array : "+avg);
+
+        ////////////////////////////////////////////////////////////
+
+
+        //int[] array = new int[] { 1, 5, 7, 4, -5 };
+
+        //int max = array.Max();
+        //Console.WriteLine(max);
+        
+
+        /////////////////////////////////////////////////////////////
+        
+        //int[] array1 = new int[] { 1, 5, -1, 16, -5 };
+
+        //int min = array1.Min();
+        //Console.WriteLine(min);
+
+        ////////////////////////////////////////////////////////////
+
+        //int size;
+        //int oddSum = 0, evenSum = 0;
+
+        //Console.WriteLine("Enter the size of the array:");
+        //size = Convert.ToInt32(Console.ReadLine());
+
+        //int[] arr = new int[size];
+
+        //Console.WriteLine("Enter the array values:");
+        //for (int i = 0; i < size; i++)
+        //{
+        //    arr[i] = Convert.ToInt32(Console.ReadLine());
+        //}
+
+        //for (int i = 0; i < size; i++)
+        //{
+        //    if (arr[i] % 2 == 0)
+        //    {
+        //        evenSum += arr[i];
+        //    }
+        //    else
+        //    {
+        //        oddSum += arr[i];
+        //    }
+            
+        //}
+        //Console.WriteLine("Even Sum: " + evenSum + " Odd Sum: " + oddSum);
     }
 }
