e45433294dbcaba0b51d9b054ffe877ff759ac62
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-10 23:35:47 -0700

    arrayse

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] a = new int[5];
+        int[] a1 = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(a1[i]);
+    }
+}
--- arrays/arrays/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int[] a = new int[5] = { 1, 2, 3, 4} ;
-       
-        Console.WriteLine(a.Length);
-    }
-}
