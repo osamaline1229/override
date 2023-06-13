63791229544c2dc5354cf4f0bb4238ed75a7cae1
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-13 03:53:36 -0700

    5oop

---
+++ oop/student.cs
@@ -0,0 +1,39 @@
+﻿using OOPBasics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopbasice
+{
+    class student
+    {
+        public long Id;
+        public string Name;
+        public double grade;
+        public string department;
+
+        public void study()
+        {
+            Console.WriteLine("student is studing");
+        }
+    }
+}
+
+
+//public student (long id, string name, string grade, string reqistrationNumber)
+//{
+//    this.id = id;
+//    this.name = name;
+//    this.grade = grade;
+//    this.department = department;
+//}
+
+//public student (long id, string name, string grade, string reqistrationNumber) : this(id, name, null, registrationNumber)
+//{
+
+
+//}
+
