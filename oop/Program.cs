7135a3883ce518751e8b87e655309e148d92aeff
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-13 03:53:08 -0700

    4oop

---
+++ oop/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace oopbasice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student st1 = new student();
+            st1.name = "osama";
+            st1.id = 123;
+            st1.grade = 3.76;
+            st1.study
+
+            Console.WriteLine(+st1.name + " " + st1.grade);
+            //the contrucsion
+        }
+    }
+
+}
+
+
+
+
+//namespace OOPBasics
+//{
+//    class student
+//    {
+//        public long id;
+//        public string name;
+//        public double grade;
+//        public string department;
+
+//        public student (long id, string name, string grade, string department)
+//        {
+//            student st1 = new student();
+//            st1.id = 234;
+//            st1.name = "salim";
+//            st1.grade = "3"; 
+//            st1.department = "engineering";
+
+//            Console.WriteLine(+st1.name + "" + id);
+
+
+//        }
+
+
+
+//        student()
+//        {
+//            Console.WriteLine(+st1.name +"" + id);
+//        }
+
+//    }
+
+
+
+
+
+
+//    void printstudentDetails()
+//    {
+//        Console.WriteLine("Name: " + this.name)
+//    }
+
+//}
+
