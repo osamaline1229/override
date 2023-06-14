365041b222935ee261a46fa53826eb92dff1a5d4
osamaline1229 <osama98138139@gmail.com>
2023-06-14 04:02:50 -0700

    game

---
+++ namespace/Program.cs
@@ -0,0 +1,38 @@
+﻿using addElements;
+using data;
+using subject;
+using System.Formats.Asn1;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        student chair = new student();
+        chair.work();
+
+
+        database dat = new database();
+        dat.order();
+
+        addition area1 = new addition();
+
+        addition.(area1);
+        
+        
+    }
+}
+
+
+namespace addElements
+{
+    class addition
+    {
+        int area = 0;
+        public void areaof(int lenght, int wieght)
+        {
+            area = lenght * wieght;
+            Console.WriteLine(area);
+        }
+    }
+}
