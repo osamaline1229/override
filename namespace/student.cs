6adc3cc5afdf7238f5d3b40d68df42f51e4da881
osamaline1229 <osama98138139@gmail.com>
2023-06-14 04:03:17 -0700

    games

---
+++ namespace/student.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+    namespace subject
+    {
+
+         class student
+        {
+            public void work()
+            {
+                Console.WriteLine("enter the value of n:");
+                Console.ReadLine();
+                Console.WriteLine("hi my freinds");
+            }
+            //the namespace
+
+        }
+    }
+    
+
+
+namespace data
+{
+    public class database
+    {
+        public void order()
+        {
+            Console.WriteLine("enter the value :");
+            Console.ReadLine();
+        }
+    }
+}
+
