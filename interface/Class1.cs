214e082ba1741ba57834d9f8d8c4dc6695bfe7d9
osamaline1229 <osama98138139@gmail.com>
2023-06-22 00:25:30 -0700

    interface

---
+++ interface/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Rectangle : Ishape
+{
+//    private double lenght;
+//    private double width;
+
+//    public Rectangle(double lenght, double width)
+//    {
+//        this.lenght = lenght;
+//        this.width = width;
+//    }
+
+//    public double CaculateArea()
+//    {
+//        return lenght * width;
+//    }
+
+//    public double CaculateParameter() 
+//    {
+//        return 2 * (lenght * width);
+//    }
+//}
+
+//public class Circle : Ishape
+//{
+//    private 
+
+    public double calculateArea()
+    {
+        return lenght;
+    }
+    public int CompareTo(Square? other)
+    {
+        if (other == null) { return 1; }
+
+        if (this.lenght > other.lenght)
+        {
+            return 1;
+        }
+        else if(this.lenght < other.lenght)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
